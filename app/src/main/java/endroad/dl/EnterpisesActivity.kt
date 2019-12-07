11923e9ca5b83e11bb551c2362cc1d60f8fc8487
RasM24 <dvolyatik@yandex.ru>
2019-12-07 13:38:14 +0700

    [datasource] Переименовал остальные упоминания factory на enterprise

--- app/src/main/java/endroad/dl/EnterpisesActivity.kt
+++ app/src/main/java/endroad/dl/EnterpisesActivity.kt
@@ -19,9 +19,9 @@ import ru.endroad.arena.data.load
 /**
  * Created by dvoly on 27.02.2017.
  */
-class FactoryActivity : ActivityExtendNavigation() {
+class EnterpisesActivity : ActivityExtendNavigation() {
 
-	override val idChecked = R.id.nav_factory
+	override val idChecked = R.id.nav_enterprise
 
 	private val enterpriseDataSource = EnterpriseDataSource()
 
@@ -33,16 +33,16 @@ class FactoryActivity : ActivityExtendNavigation() {
 		val linearLayoutManager = LinearLayoutManager(this)
 		recycleList.addItemDecoration(DividerItemDecoration(baseContext, linearLayoutManager.orientation))
 		recycleList.layoutManager = linearLayoutManager
-		recycleList.adapter = FactoryAdapter(enterpriseDataSource.getList(), this)
+		recycleList.adapter = EnterpriseAdapter(enterpriseDataSource.getList(), this)
 	}
 
 	override fun onClick(view: View) {}
-	private inner class FactoryAdapter internal constructor(val enterpriseList: List<Enterprise>, val context: Context) :
-		RecyclerView.Adapter<FactoryAdapter.ViewHolder>() {
+	private inner class EnterpriseAdapter internal constructor(val enterpriseList: List<Enterprise>, val context: Context) :
+		RecyclerView.Adapter<EnterpriseAdapter.ViewHolder>() {
 
 		internal inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
 
-			val cv: View = itemView.findViewById(R.id.factory_view)
+			val cv: View = itemView.findViewById(R.id.enterprise_item)
 			val image: ImageView = itemView.findViewById(R.id.image)
 			val name: TextView = itemView.findViewById(R.id.text_name)
 
@@ -53,8 +53,8 @@ class FactoryActivity : ActivityExtendNavigation() {
 			override fun onClick(view: View) {
 				val position = adapterPosition
 				if (position != RecyclerView.NO_POSITION) {
-					val intent = Intent(applicationContext, FactoryInfoActivity::class.java)
-					intent.putExtra("factoryId", position)
+					val intent = Intent(applicationContext, EnterpriseInfoActivity::class.java)
+					intent.putExtra("enterpriseId", position)
 					startActivity(intent)
 				}
 			}
@@ -62,7 +62,7 @@ class FactoryActivity : ActivityExtendNavigation() {
 		}
 
 		override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
-			val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.item_factory, viewGroup, false)
+			val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.item_enterprise, viewGroup, false)
 			return ViewHolder(v)
 		}
 
--- app/src/main/java/endroad/dl/EnterpriseInfoActivity.kt
+++ app/src/main/java/endroad/dl/EnterpriseInfoActivity.kt
@@ -4,25 +4,25 @@ import android.app.Activity
 import android.os.Bundle
 import android.text.Html
 import endroad.dl.data.EnterpriseDataSource
-import kotlinx.android.synthetic.main.dialog_factory_info.*
+import kotlinx.android.synthetic.main.dialog_enterprise_info.*
 import ru.endroad.arena.data.load
 
 /**
  * Created by dvoly on 28.02.2017.
  */
-class FactoryInfoActivity : Activity() {
+class EnterpriseInfoActivity : Activity() {
 
 	private val enterpriseDataSource = EnterpriseDataSource()
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.dialog_factory_info)
+		setContentView(R.layout.dialog_enterprise_info)
 
 		//TODO нет обработки ошибки NoSuchElementException
-		val factory = enterpriseDataSource.getByPosition(intent.getIntExtra("factoryId", -1))
+		val enterprise = enterpriseDataSource.getByPosition(intent.getIntExtra("enterpriseId", -1))
 
-		text_name.text = factory.name
-		text_information.text = Html.fromHtml(factory.information)
-		image.load(factory.imagePath)
+		text_name.text = enterprise.name
+		text_information.text = Html.fromHtml(enterprise.information)
+		image.load(enterprise.imagePath)
 	}
 }
