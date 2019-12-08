802665ca0a3b2f50068bb3022c243f0adff41f65
RasM24 <dvolyatik@yandex.ru>
2019-12-08 14:16:19 +0700

    [fastadapter] Использовал базовый ListFragment для предприятий

--- app/src/main/java/endroad/dl/view/EnterprisesFragment.kt
+++ app/src/main/java/endroad/dl/view/EnterprisesFragment.kt
@@ -1,75 +1,42 @@
 package endroad.dl.view
 
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.mikepenz.fastadapter.IModelItem
 import endroad.dl.R
 import endroad.dl.application.SingleActivity
 import endroad.dl.data.EnterpriseDataSource
 import endroad.dl.models.Enterprise
-import kotlinx.android.synthetic.main.activity_list.*
-import ru.endroad.arena.data.load
-import ru.endroad.arena.viewlayer.fragment.BaseFragment
+import endroad.dl.view.item.EnterpriseItem
+import ru.endroad.arena.viewlayer.fragment.ListFragment
 
-class EnterprisesFragment : BaseFragment() {
+class EnterprisesFragment : ListFragment() {
 
 	private val enterpriseDataSource = EnterpriseDataSource()
 
 	override val layout = R.layout.base_fragment_list
 
 	//Избыточность BaseFragment.. Уберется в будущем из модуля Arena
-	override fun setupViewModel() {
-		val mLayoutManager = LinearLayoutManager(requireContext())
-		list.setHasFixedSize(true)
-		list.layoutManager = mLayoutManager
-		list.addItemDecoration(DividerItemDecoration(requireContext(), mLayoutManager.orientation))
-		list.adapter = EnterpriseAdapter(enterpriseDataSource.getList())
+	override fun setupViewModel() {}
+
+	override fun setupViewComponents() {
+		setDivider(R.drawable.divider)
+		enterpriseDataSource.getList()
+			.map(::EnterpriseItem)
+			.setItems()
+	}
+
+	override fun onClickItem(item: IModelItem<*, *>): Boolean {
+		val enterprise = item.model as? Enterprise ?: return false
+
+		//TODO костыль, но пусть так и останется в этом проекте :)
+		val position = enterpriseDataSource.getList().indexOf(enterprise)
+		(requireActivity() as SingleActivity).toEnterpriseInfo(position)
+
+		return true
 	}
 
 	companion object {
 		fun newInstance(): Fragment = EnterprisesFragment()
-	}
-
-	private inner class EnterpriseAdapter internal constructor(val enterpriseList: List<Enterprise>) :
-		RecyclerView.Adapter<EnterpriseAdapter.ViewHolder>() {
-
-		internal inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
-
-			val cv: View = itemView.findViewById(R.id.enterprise_item)
-			val image: ImageView = itemView.findViewById(R.id.image)
-			val name: TextView = itemView.findViewById(R.id.text_name)
-
-			init {
-				cv.setOnClickListener(this)
-			}
-
-			override fun onClick(view: View) {
-				val position = adapterPosition
-				if (position != RecyclerView.NO_POSITION) {
-					(requireActivity() as SingleActivity).toEnterpriseInfo(position)
-				}
-			}
-		}
-
-		override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
-			val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.item_enterprise, viewGroup, false)
-			return ViewHolder(v)
-		}
-
-		override fun onBindViewHolder(ViewHolder: ViewHolder, i: Int) {
-			ViewHolder.name.text = enterpriseList[i].name
-			ViewHolder.image.load(enterpriseList[i].imagePath)
-		}
-
-		override fun getItemCount(): Int {
-			return enterpriseList.size
-		}
 	}
 }
 
---
+++ app/src/main/java/endroad/dl/view/item/EnterpriseItem.kt
@@ -0,0 +1,32 @@
+package endroad.dl.view.item
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.mikepenz.fastadapter.items.ModelAbstractItem
+import endroad.dl.R
+import endroad.dl.models.Enterprise
+import ru.endroad.arena.data.CircleTransform
+import ru.endroad.arena.data.load
+
+class EnterpriseItem(item: Enterprise) : ModelAbstractItem<Enterprise, EnterpriseItem.ViewHolder>(item) {
+
+	override val type = R.id.item_enterprise
+
+	override val layoutRes = R.layout.item_enterprise
+
+	override fun getViewHolder(v: View) = ViewHolder(v)
+
+	override fun bindView(holder: ViewHolder, payloads: MutableList<Any>) {
+		super.bindView(holder, payloads)
+
+		holder.name.text = model.name
+		holder.image.load(model.imagePath)
+	}
+
+	class ViewHolder(root: View) : RecyclerView.ViewHolder(root) {
+		val image: ImageView = root.findViewById(R.id.image)
+		val name: TextView = root.findViewById(R.id.text_name)
+	}
+}
