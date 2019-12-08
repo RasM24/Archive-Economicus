7d66d319304a1e251e963229727f982a03f37fcf
RasM24 <dvolyatik@yandex.ru>
2019-12-08 13:49:18 +0700

    [fastadapter] Использовал базовый ListFragment для контактов

--- app/src/main/java/endroad/dl/view/ContactFragment.kt
+++ app/src/main/java/endroad/dl/view/ContactFragment.kt
@@ -2,79 +2,40 @@ package endroad.dl.view
 
 import android.content.Intent
 import android.net.Uri
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.mikepenz.fastadapter.IModelItem
 import endroad.dl.R
 import endroad.dl.data.ContactDataSource
 import endroad.dl.models.Contact
-import kotlinx.android.synthetic.main.activity_list.*
-import ru.endroad.arena.data.CircleTransform
-import ru.endroad.arena.data.load
-import ru.endroad.arena.viewlayer.fragment.BaseFragment
+import endroad.dl.view.item.ContactItem
+import ru.endroad.arena.viewlayer.fragment.ListFragment
 
-class ContactFragment : BaseFragment() {
+class ContactFragment : ListFragment() {
 
 	override val layout = R.layout.base_fragment_list
 
 	private val contactDataSource = ContactDataSource()
 
 	//Избыточность BaseFragment.. Уберется в будущем из модуля Arena
-	override fun setupViewModel() {
-		val mLayoutManager = LinearLayoutManager(requireContext())
-		list.setHasFixedSize(true)
-		list.layoutManager = mLayoutManager
-		list.addItemDecoration(DividerItemDecoration(requireContext(), mLayoutManager.orientation))
-		list.adapter = LinkAdapter(contactDataSource.getContactList())
+	override fun setupViewModel() {}
+
+	override fun setupViewComponents() {
+		setDivider(R.drawable.divider)
+		contactDataSource.getContactList()
+			.map(::ContactItem)
+			.setItems()
+	}
+
+	override fun onClickItem(item: IModelItem<*, *>): Boolean {
+		val model = item.model as? Contact ?: return false
+		val browser = Intent(Intent.ACTION_VIEW, Uri.parse(model.url))
+		startActivity(browser)
+
+		return true
 	}
 
 	companion object {
 		fun newInstance(): Fragment = ContactFragment()
-	}
-
-	private inner class LinkAdapter(val list: List<Contact>) :
-		RecyclerView.Adapter<LinkAdapter.ViewHolder>() {
-
-		internal inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
-			val cv: View = itemView.findViewById(R.id.contact_item)
-			val image: ImageView = itemView.findViewById(R.id.image)
-			val post: TextView = itemView.findViewById(R.id.name)
-			val name: TextView = itemView.findViewById(R.id.post)
-
-			init {
-				cv.setOnClickListener(this)
-			}
-
-			override fun onClick(view: View) {
-				val position = adapterPosition
-				if (position != RecyclerView.NO_POSITION) {
-					val address = Uri.parse(list[position].url)
-					val browser = Intent(Intent.ACTION_VIEW, address)
-					startActivity(browser)
-				}
-			}
-
-		}
-
-		override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
-			val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.item_contact, viewGroup, false)
-			return ViewHolder(v)
-		}
-
-		override fun onBindViewHolder(ViewHolder: ViewHolder, i: Int) {
-			ViewHolder.name.text = list[i].name
-			ViewHolder.post.text = list[i].post
-			ViewHolder.image.load(list[i].imagePath, CircleTransform())
-		}
-
-		override fun getItemCount(): Int =
-			list.size
 	}
 }
 
---
+++ app/src/main/java/endroad/dl/view/item/ContactItem.kt
@@ -0,0 +1,34 @@
+package endroad.dl.view.item
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.mikepenz.fastadapter.items.ModelAbstractItem
+import endroad.dl.R
+import endroad.dl.models.Contact
+import ru.endroad.arena.data.CircleTransform
+import ru.endroad.arena.data.load
+
+class ContactItem(item: Contact) : ModelAbstractItem<Contact, ContactItem.ViewHolder>(item) {
+
+	override val type = R.id.item_contact
+
+	override val layoutRes = R.layout.item_contact
+
+	override fun getViewHolder(v: View) = ViewHolder(v)
+
+	override fun bindView(holder: ViewHolder, payloads: MutableList<Any>) {
+		super.bindView(holder, payloads)
+
+		holder.name.text = model.name
+		holder.post.text = model.post
+		holder.image.load(model.imagePath, CircleTransform())
+	}
+
+	class ViewHolder(root: View) : RecyclerView.ViewHolder(root) {
+		val image: ImageView = root.findViewById(R.id.image)
+		val post: TextView = root.findViewById(R.id.name)
+		val name: TextView = root.findViewById(R.id.post)
+	}
+}
