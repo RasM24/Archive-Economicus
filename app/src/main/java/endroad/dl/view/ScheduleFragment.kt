6c409d794aba964963d7a874ad4b13202879a085
RasM24 <dvolyatik@yandex.ru>
2019-12-08 20:29:28 +0700

    [fastadapter] Использовал базовый ListFragment для расписания

--- app/src/main/java/endroad/dl/view/ScheduleFragment.kt
+++ app/src/main/java/endroad/dl/view/ScheduleFragment.kt
@@ -1,44 +1,37 @@
 package endroad.dl.view
 
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.firebase.ui.database.FirebaseRecyclerAdapter
-import com.google.firebase.database.Query
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import endroad.dl.R
-import endroad.dl.ViewHolders.*
 import endroad.dl.databaseReference
 import endroad.dl.models.Schedule
-import kotlinx.android.synthetic.main.activity_list.*
-import ru.endroad.arena.viewlayer.fragment.BaseFragment
+import endroad.dl.view.item.ScheduleItem
+import ru.endroad.arena.viewlayer.fragment.ListFragment
 
-//TODO перевести на ListFragment
-class ScheduleFragment : BaseFragment() {
+class ScheduleFragment : ListFragment() {
 
-	private val query: Query = databaseReference.child(Schedule.SCHEDULE_COMMON)
-
-	private val firebaseAdapter = object :
-		FirebaseRecyclerAdapter<Schedule, ScheduleHolder>(Schedule::class.java, R.layout.item_shedule, ScheduleHolder::class.java, query) {
-		public override fun populateViewHolder(vh: ScheduleHolder, schedule: Schedule, position: Int) {
-			vh.bindData(schedule)
+	private val postListener = object : ValueEventListener {
+		override fun onDataChange(dataSnapshot: DataSnapshot) {
+			dataSnapshot
+				.children
+				.reversed()
+				.mapNotNull { it.getValue(Schedule::class.java) }
+				.map(::ScheduleItem)
+				.setItems()
 		}
+
+		override fun onCancelled(databaseError: DatabaseError) {}
 	}
 
-	override val layout = R.layout.base_fragment_list
-
 	//Избыточность BaseFragment.. Уберется в будущем из модуля Arena
-	override fun setupViewModel() {
+	override fun setupViewModel() {}
 
-		val linearLayoutManager = LinearLayoutManager(requireContext()).apply {
-			reverseLayout = true
-			stackFromEnd = true
-		}
+	override fun setupViewComponents() {
+		databaseReference.child(Schedule.SCHEDULE_COMMON).addListenerForSingleValueEvent(postListener)
 
-		list.setHasFixedSize(true)
-		list.layoutManager = linearLayoutManager
-		list.addItemDecoration(DividerItemDecoration(requireContext(), linearLayoutManager.orientation))
-
-		list.adapter = firebaseAdapter
+		setDivider(R.drawable.divider)
 	}
 
 	companion object {
---
+++ app/src/main/java/endroad/dl/view/item/ScheduleItem.kt
@@ -0,0 +1,27 @@
+package endroad.dl.view.item
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.mikepenz.fastadapter.items.ModelAbstractItem
+import endroad.dl.R
+import endroad.dl.models.Schedule
+
+class ScheduleItem(item: Schedule) : ModelAbstractItem<Schedule, ScheduleItem.ViewHolder>(item) {
+
+	override val type = R.id.item_shedule
+
+	override val layoutRes = R.layout.item_shedule
+
+	override fun getViewHolder(v: View) = ViewHolder(v)
+
+	override fun bindView(holder: ViewHolder, payloads: MutableList<Any>) {
+		super.bindView(holder, payloads)
+
+		holder.mTextField.text = model.text
+	}
+
+	class ViewHolder(root: View) : RecyclerView.ViewHolder(root) {
+		val mTextField: TextView = itemView.findViewById(R.id.text_information)
+	}
+}
